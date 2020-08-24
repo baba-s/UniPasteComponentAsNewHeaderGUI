a1535cdc13705a7191df049031f2f44c14d4d75b
baba_s <shotababa1206@gmail.com>
2020-08-24 20:20:24 +0900

    first commit

---
+++ Editor/PasteComponentAsNewHeaderGUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[InitializeOnLoad]
+	internal static class PasteComponentAsNewHeaderGUI
+	{
+		static PasteComponentAsNewHeaderGUI()
+		{
+			Editor.finishedDefaultHeaderGUI += OnGUI;
+		}
+
+		private static void OnGUI( Editor editor )
+		{
+			var gameObject = editor.target as GameObject;
+
+			// ゲームオブジェクトの Inspector ではない場合は無視
+			if ( gameObject == null ) return;
+
+			// プレハブの場合は無視
+			if ( string.IsNullOrWhiteSpace( gameObject.scene.path ) ) return;
+
+			if ( GUILayout.Button( "Paste Component As New" ) )
+			{
+				ComponentUtility.PasteComponentAsNew( gameObject );
+			}
+		}
+	}
+}
